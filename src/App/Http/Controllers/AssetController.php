16dcf2f2ab9c106e500aecefda6151a3ecb852ec
Muhammad Jawad <mjawad096@gmail.com>
2021-07-01 17:49:50 +0500

    increased image size to 100MB

--- src/App/Http/Controllers/AssetController.php
+++ src/App/Http/Controllers/AssetController.php
@@ -29,7 +29,7 @@ class AssetController extends Controller
     {
         $this->validate($request,[
             'file' => 'bail|required|array|min:1',
-            'file.*' => 'bail|required|max:10240'
+            'file.*' => 'bail|required|max:102400'
         ]);
 
         $media = TempMedia::create()->addMediaFromRequest('file')->toMediaCollection('default');
