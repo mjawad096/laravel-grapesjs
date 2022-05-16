ae80ee7036ae075a671cf5bc96d0cbeab2676e5e
Muhammad Jawad <mjawad096@gmail.com>
2022-05-16 14:19:22 +0500

    Updated editor blade file and editor trait

--- src/resources/views/edittor.blade.php
+++ src/resources/views/edittor.blade.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit {{ $model->editor_page_title }}</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/laravel-grapesjs/assets/editor.css') }}">
     <style>
         * {
             margin: 0;
@@ -20,10 +22,8 @@
 </head>
 
 <body>
-    <div id="loader" style="left: 0;top:0;background-color:white;opacity:0.7;position:absolute;align-items:center;justify-content:center;width:100%;height:100%;font-size:36pt;display:flex;z-index:100;">
-        <i class="fa fa-spinner fa-spin"></i>
-    </div>
     <div id="{{ str_replace('#', '', $editorConfig->container ?? 'editor') }}"></div>
-    <script src="{{ asset('vendor/grapesjs/editor.js') }}"></script>
+
+    <script src="{{ asset('vendor/laravel-grapesjs/assets/editor.js') }}"></script>
 </body>
 </html>
