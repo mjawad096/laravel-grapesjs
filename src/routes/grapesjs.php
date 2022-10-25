6308340b2b79bcae829a0340e8e719056cfba686
Muhammad Jawad <jawad.topdot@gmail.com>
2022-10-25 16:08:21 +0500

    Protected the routes with middlewares and added middleware option in config

--- src/routes/grapesjs.php
+++ src/routes/grapesjs.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('grapesjs')->name('grapesjs.')->namespace('Dotlogics\Grapesjs\App\Http\Controllers')->group(function(){
-	Route::post('editor/asset/store', 'AssetController@store')->name('editor.asset.store');
-	
-	Route::get('editor/{model}/{editable}', 'EditorController@editor')->name('editor.model.editor');
-	Route::post('editor/{model}/{editable}', 'EditorController@store')->name('editor.model.store');
-	
-	Route::get('editor/{model}/{editable}/templates', 'EditorController@templates')->name('editor.model.templates');
-	Route::get('editor/templates', 'EditorController@templates')->name('editor.templates');
-});
+Route::prefix('grapesjs')
+	->name('grapesjs.')
+	->namespace('Dotlogics\Grapesjs\App\Http\Controllers')
+	->middleware(config('grapesjs.middleware', []))
+	->group(function(){
+		Route::post('editor/asset/store', 'AssetController@store')->name('editor.asset.store');
+		
+		Route::get('editor/{model}/{editable}', 'EditorController@editor')->name('editor.model.editor');
+		Route::post('editor/{model}/{editable}', 'EditorController@store')->name('editor.model.store');
+		
+		Route::get('editor/{model}/{editable}/templates', 'EditorController@templates')->name('editor.model.templates');
+		Route::get('editor/templates', 'EditorController@templates')->name('editor.templates');
+	});
